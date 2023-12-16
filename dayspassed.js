c86eac02d8c82839ddc5caecb307787add118f02
garozaev <g.a.rozaev@gmail.com>
2023-12-16 19:26:27 +0300

    finish

--- dayspassed.js
+++ dayspassed.js
@@ -1,16 +1,24 @@
-
+//забираем input из HTML
 const messageDate = document.querySelector('#messageDate');
-const resultInput = document.querySelector('#result')
+//забираем p из HTML
+const resultP = document.querySelector('#result');
+//забираем button из HTML
 const button = document.querySelector('#button_calculate'); 
 
 
 const colculeteDaysPassed = () => {
+    // забираем значение дату из input
     const massegeDateStr = messageDate.value;
+    // преобразуем в милисекунды
     const massegeTimestamp = Date.parse(massegeDateStr);
+    // получаем текущую дату в милисекундах
     const currentTimestemp = Date.now();
+    // вычисляем разницу между выброной и текущей
     const timeDifferenc = massegeTimestamp - currentTimestemp;
+    // переводим в дни и округляем
     const datesPassed = Math.floor(timeDifferenc / (1000 * 60 * 60 * 24));
-    resultInput.textContent = `Passed ${datesPassed} days`;
+    // передаём результат переменной для вывода на экран
+    resultP.textContent = `Passed ${datesPassed} days`;
 };
 
 button.addEventListener('click', colculeteDaysPassed);
